5334ded0b18175c80aff6d62ce432da4b0af465c
yulin dima <yulin@microline.ru>
2022-06-15 21:30:50 +0300

    created crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user/entities/user.entity';
+import { PostModule } from './post/post.module';
+import { PostEntity } from './post/entities/post.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '0000',
+      database: 'tjournal',
+      entities: [UserEntity, PostEntity],
+      synchronize: true,
+    }),
+    UserModule,
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
