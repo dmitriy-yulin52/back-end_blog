eb38db5739172b8aad75a5ead3736be09645e663
yulin dima <yulin_dimon@mail.ru>
2022-07-22 16:25:42 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: [/^(.*)/],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+    credentials: true,
+    allowedHeaders:
+      'Origin,X-Requested0with,Content-type,Accept,Authorization,authorization,X-Forwarded-for',
+  });
   await app.listen(7777);
 }
 
